04e914d1152e6facedefeb601f6af626bc066f3f
AnPham <anphambk@gmail.com>
2017-06-18 12:52:58 +0800

    fdsg

--- sw/include/final.h
+++ sw/include/final.h
@@ -3,6 +3,14 @@
 
 #include "hdmi.h"
 #include "v4l2grab.h"
-#include "image.h"
+
+FILE          *fp_s = NULL;    // source file handler
+unsigned int  width, height;   // image width, image height
+unsigned char *image_s = NULL; // source image array
+unsigned int rgb_raw_data_offset; // RGB raw data offset
+unsigned short bit_per_pixel;     // bit per pixel
+unsigned short byte_per_pixel;    // byte per pixel
+int x_max;
+int y_max;
 
 #endif /* __FINAL_H__ */
--- sw/src/final.c
+++ sw/src/final.c
@@ -1,5 +1,30 @@
 #include "final.h"
 
+void init_image (char* fname) {
+  fp_s = fopen(fname, "rb");
+  if (fp_s == NULL) {
+    printf("fopen fp_s error\n");
+    return;
+  }
+  // move offset to 10 to find rgb raw data offset
+  fseek(fp_s, 10, SEEK_SET);
+  fread(&rgb_raw_data_offset, sizeof(unsigned int), 1, fp_s);
+  // move offset to 18    to get width & height;
+  fseek(fp_s, 18, SEEK_SET);
+  fread(&width,  sizeof(unsigned int), 1, fp_s);
+  fread(&height, sizeof(unsigned int), 1, fp_s);
+  // get  bit per pixel
+  fseek(fp_s, 28, SEEK_SET);
+  fread(&bit_per_pixel, sizeof(unsigned short), 1, fp_s);
+  byte_per_pixel = bit_per_pixel / 8;
+  // move offset to rgb_raw_data_offset to get RGB raw data
+  fseek(fp_s, rgb_raw_data_offset, SEEK_SET);
+
+  image_s = (unsigned char *)malloc((size_t)width * height * byte_per_pixel);
+  if (image_s == NULL) {
+  printf("malloc images_s error\n");
+}
+
 void process(void)
 {
     // 6. Image processing///////////////////////////////
